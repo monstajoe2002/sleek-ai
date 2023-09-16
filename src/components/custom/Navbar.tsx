497beb8a313f9c82b4e43057fe9be8ace0384d23
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-09-16 23:43:17 +0300

    popover added around profile image

--- src/components/custom/Navbar.tsx
+++ src/components/custom/Navbar.tsx
@@ -5,6 +5,7 @@ import { Button } from "../ui/button";
 import useStoreUserEffect from "@/hooks/useStoreUserEffect";
 import { SignInButton, SignOutButton } from "@clerk/clerk-react";
 import Image from "next/image";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 export default function Navbar() {
   const { userId, user } = useStoreUserEffect();
@@ -19,16 +20,25 @@ export default function Navbar() {
       </Link>
       {userId ? (
         <div className="flex flex-row gap-2">
-          <Button>
-            <SignOutButton />
-          </Button>
-          <Image
-            src={user!.imageUrl}
-            width={50}
-            height={50}
-            className="rounded-full"
-            alt="profile pic"
-          />
+          <Popover>
+            <PopoverTrigger>
+              <Image
+                src={user!.imageUrl}
+                width={50}
+                height={50}
+                className="rounded-full"
+                alt="profile pic"
+              />
+            </PopoverTrigger>
+            <PopoverContent>
+              <h4 className="font-medium leading-none mb-4">
+                Welcome, {user?.firstName}
+              </h4>
+              <Button>
+                <SignOutButton />
+              </Button>
+            </PopoverContent>
+          </Popover>
         </div>
       ) : (
         <Button>
