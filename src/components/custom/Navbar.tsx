45c6bed53bacd7a6122177a314b846a77f8b4f91
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-09-16 23:29:00 +0300

    add profile picture to navbar

--- src/components/custom/Navbar.tsx
+++ src/components/custom/Navbar.tsx
@@ -4,19 +4,37 @@ import React from "react";
 import { Button } from "../ui/button";
 import useStoreUserEffect from "@/hooks/useStoreUserEffect";
 import { SignInButton, SignOutButton } from "@clerk/clerk-react";
+import Image from "next/image";
 
 export default function Navbar() {
   const { userId, user } = useStoreUserEffect();
 
   return (
-    <div className="py-6 justify-between">
+    <div className="py-6 justify-between flex">
       <Link
         href="/"
         className="scroll-m-20 text-4xl font-bold tracking-tight 
       bg-clip-text bg-gradient-to-r text-transparent from-blue-500 to-blue-800 ">
         Sleek
-        <Button>{userId ? <SignOutButton /> : <SignInButton />}</Button>
       </Link>
+      {userId ? (
+        <div className="flex flex-row gap-2">
+          <Button>
+            <SignOutButton />
+          </Button>
+          <Image
+            src={user!.imageUrl}
+            width={50}
+            height={50}
+            className="rounded-full"
+            alt="profile pic"
+          />
+        </div>
+      ) : (
+        <Button>
+          <SignInButton />
+        </Button>
+      )}
     </div>
   );
 }
