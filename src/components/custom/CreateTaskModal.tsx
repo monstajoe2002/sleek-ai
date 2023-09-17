841d7d706240d8eac11d89df1d1b10e1f7f20527
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-09-18 00:47:32 +0300

    basic form setup

--- src/components/custom/CreateTaskModal.tsx
+++ src/components/custom/CreateTaskModal.tsx
@@ -1,23 +1,95 @@
-import React, { ComponentProps } from "react";
+"use client";
+import React from "react";
 import {
   Dialog,
   DialogContent,
   DialogTitle,
   DialogTrigger,
 } from "../ui/dialog";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "../ui/button";
 import { Plus } from "lucide-react";
-type Props = ComponentProps<typeof Button>;
-export default function CreateTaskModal({ className }: Props) {
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+const formSchema = z.object({
+  title: z.string().min(1),
+  description: z.string().optional(),
+  occurence: z.number().min(1).max(7),
+  completed: z.boolean(),
+  date: z.date().optional(),
+});
+export default function CreateTaskModal() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      occurence: 1,
+      completed: false,
+    },
+  });
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
   return (
     <Dialog>
-      <DialogTrigger>
-        <Button className="w-full mt-2" variant={"secondary"}>
+      <DialogTrigger asChild>
+        <Button className="w-full mt-2" variant={"secondary"} size="icon">
           <Plus className="h-4 w-4" />
         </Button>
       </DialogTrigger>
       <DialogContent>
         <DialogTitle>Create New Task</DialogTitle>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input required={true} {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="occurence"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Number of days</FormLabel>
+                  <FormControl>
+                    <Input type="number" max={7} min={1} {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
