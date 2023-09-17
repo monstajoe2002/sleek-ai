d07090b921c85fb2424c813aa82589cbe01ce0aa
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-09-17 12:58:38 +0300

    style headings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,16 @@ import { SignInButton } from "@clerk/clerk-react";
 export default function Home() {
   const weekdays = useWeekdays();
   return (
-    <div>
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
       {weekdays.map((day) => (
-        <div key={day.toISOString()}>
-          {new Intl.DateTimeFormat("en-US", {
-            day: "2-digit",
-            weekday: "long",
-            month: "long",
-          }).format(day)}
+        <div className="flex flex-col items-center" key={day.toISOString()}>
+          <h3 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-blue-400">
+            {new Intl.DateTimeFormat("en-US", {
+              day: "2-digit",
+              weekday: "long",
+              month: "long",
+            }).format(day)}
+          </h3>
         </div>
       ))}
     </div>
