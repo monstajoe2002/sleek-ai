f85373502f681d507cde5c671c497f51eff198ec
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-09-17 17:49:26 +0300

    align main content in the center vertically

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import useWeekdays from "@/hooks/useWeekdays";
 export default function Home() {
   const weekdays = useWeekdays();
   return (
-    <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-8">
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 my-auto">
       {weekdays.map((day) => (
         <div className="flex flex-col items-center" key={day.toISOString()}>
           <h3 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-blue-400">
