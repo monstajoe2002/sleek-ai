d491806db7f1adbdbeddc40d834f688fa515a284
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-09-19 01:11:40 +0300

    skeleton now shows on loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,22 @@
 
 import CreateTaskModal from "@/components/custom/CreateTaskModal";
 import Task from "@/components/custom/Task";
+import { Skeleton } from "@/components/ui/skeleton";
 import useWeekdays from "@/hooks/useWeekdays";
 import { api } from "@convex/_generated/api";
 import { useQuery } from "convex/react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
   const { formattedWeekDays, today } = useWeekdays();
   const getTasksQuery = useQuery(api.tasks.getAllTasks);
+  useEffect(() => {
+    setIsLoading(true);
+    if (getTasksQuery) {
+      setIsLoading(false);
+    }
+  }, [getTasksQuery]);
   return (
     <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 my-auto">
       {formattedWeekDays.map((day, i) => (
@@ -16,11 +25,15 @@ export default function Home() {
           <h3 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-blue-400">
             {day}
           </h3>
-          {getTasksQuery?.map((task) => {
-            if (task.date === day) {
-              return <Task key={task._id} title={task.title} id={task._id} />;
-            }
-          })}
+          {isLoading ? (
+            <Skeleton className="p-4 mt-4 mb-6 w-full h-[50px]" />
+          ) : (
+            getTasksQuery?.map((task) => {
+              if (task.date === day) {
+                return <Task key={task._id} title={task.title} id={task._id} />;
+              }
+            })
+          )}
           <CreateTaskModal dueDate={day} />
         </div>
       ))}
