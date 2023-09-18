ab05ef7ec85ec735ff0989c81c19eee0c2afbc3a
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-09-19 01:31:59 +0300

    fix hydration error

--- src/components/custom/ThemeToggle.tsx
+++ src/components/custom/ThemeToggle.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "../ui/button";
 export default function ThemeToggle() {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
   return (
     <Button
       onClick={() => (theme === "dark" ? setTheme("light") : setTheme("dark"))}
