0faa592b00f092a6a646ea68dc5d11c30746ca95
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-09-19 14:28:02 +0300

    create function to get auth details

--- convex/users.ts
+++ convex/users.ts
@@ -1,4 +1,4 @@
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 /**
  * Insert or update the user in a Convex table then return the document's ID.
@@ -39,5 +39,25 @@ export const store = mutation({
       name: identity.name!,
       tokenIdentifier: identity.tokenIdentifier,
     });
+  },
+});
+
+export const getLoggedInUser = query({
+  args: {},
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      return null;
+    }
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_token", (q) =>
+        q.eq("tokenIdentifier", identity.tokenIdentifier)
+      )
+      .unique();
+    if (!user) {
+      return null;
+    }
+    return user;
   },
 });
