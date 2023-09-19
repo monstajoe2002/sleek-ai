a12001adcc2467bdf5bfec00735342e459b89dd4
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-09-19 12:43:52 +0300

    setup react-beautiful-dnd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,16 @@ import useWeekdays from "@/hooks/useWeekdays";
 import { api } from "@convex/_generated/api";
 import { useQuery } from "convex/react";
 import { useEffect, useState } from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
   const { formattedWeekDays, today } = useWeekdays();
   const getTasksQuery = useQuery(api.tasks.getAllTasks);
+  const onDragEnd = (result: any) => {
+    // TODO: handle drag and drop
+  };
+
   useEffect(() => {
     setIsLoading(true);
     if (getTasksQuery) {
@@ -19,24 +24,39 @@ export default function Home() {
     }
   }, [getTasksQuery]);
   return (
-    <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 my-auto">
-      {formattedWeekDays.map((day, i) => (
-        <div className="flex flex-col" key={i}>
-          <h3 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-blue-400">
-            {day}
-          </h3>
-          {isLoading ? (
-            <Skeleton className="p-4 mt-4 mb-6 w-full h-[50px]" />
-          ) : (
-            getTasksQuery?.map((task) => {
-              if (task.date === day) {
-                return <Task key={task._id} title={task.title} id={task._id} />;
-              }
-            })
-          )}
-          <CreateTaskModal dueDate={day} />
-        </div>
-      ))}
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 my-auto">
+        {formattedWeekDays.map((day, i) => (
+          <div className="flex flex-col" key={i}>
+            <h3 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-blue-400">
+              {day}
+            </h3>
+            <Droppable droppableId={day}>
+              {({ innerRef, droppableProps }) => (
+                <div ref={innerRef} {...droppableProps}>
+                  {isLoading ? (
+                    <Skeleton className="p-4 mt-4 mb-6 w-full h-[50px]" />
+                  ) : (
+                    getTasksQuery?.map((task, i) => {
+                      if (task.date === day) {
+                        return (
+                          <Task
+                            index={i}
+                            key={task._id}
+                            title={task.title}
+                            id={task._id}
+                          />
+                        );
+                      }
+                    })
+                  )}
+                </div>
+              )}
+            </Droppable>
+            <CreateTaskModal dueDate={day} />
+          </div>
+        ))}
+      </div>
+    </DragDropContext>
   );
 }
--- src/components/custom/Task.tsx
+++ src/components/custom/Task.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import { Card, CardDescription } from "../ui/card";
 import { Id } from "@convex/_generated/dataModel";
@@ -13,34 +11,44 @@ import {
 import { Trash } from "lucide-react";
 import { api } from "@convex/_generated/api";
 import { useMutation } from "convex/react";
+import { Draggable } from "react-beautiful-dnd";
 
 type Props = {
   id: Id<"tasks">;
   title: string;
+  index: number;
 };
 
-export default function Task({ title, id }: Props) {
+export default function Task({ title, id, index }: Props) {
   const deleteTaskMutation = useMutation(api.tasks.deleteTask);
   const deleteTask = () => {
     deleteTaskMutation({ id });
   };
   return (
-    <ContextMenu>
-      <ContextMenuTrigger>
-        <EditTaskModal taskId={id}>
-          <Card className="p-4 mt-4 mb-6 cursor-pointer hover:shadow-md transition-shadow">
-            <CardDescription className="text-base">{title}</CardDescription>
-          </Card>
-        </EditTaskModal>
-      </ContextMenuTrigger>
-      <ContextMenuContent>
-        <ContextMenuItem
-          className="focus:bg-red-100 focus:text-red-600 text-red-600"
-          onClick={deleteTask}>
-          <Trash className="h-4 w-4 mr-2" />
-          Delete
-        </ContextMenuItem>
-      </ContextMenuContent>
-    </ContextMenu>
+    <Draggable draggableId={id} index={index}>
+      {(provided) => (
+        <ContextMenu>
+          <ContextMenuTrigger>
+            <EditTaskModal taskId={id}>
+              <Card
+                className="p-4 mt-4 mb-6 cursor-pointer hover:shadow-md transition-shadow"
+                ref={provided.innerRef}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}>
+                <CardDescription className="text-base">{title}</CardDescription>
+              </Card>
+            </EditTaskModal>
+          </ContextMenuTrigger>
+          <ContextMenuContent>
+            <ContextMenuItem
+              className="focus:bg-red-100 focus:text-red-600 text-red-600"
+              onClick={deleteTask}>
+              <Trash className="h-4 w-4 mr-2" />
+              Delete
+            </ContextMenuItem>
+          </ContextMenuContent>
+        </ContextMenu>
+      )}
+    </Draggable>
   );
 }
